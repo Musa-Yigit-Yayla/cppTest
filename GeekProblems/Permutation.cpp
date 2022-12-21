4225628a6387a2cce26e37767da5a0b3ef171316
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2022-12-21 12:00:18 +0300

    Some more adjustments made
    Still problematic, but works better

--- GeekProblems/Permutation.cpp
+++ GeekProblems/Permutation.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 #include <string>
 #include <cmath>
-
+#include <ctime>
+#include <random>
 using namespace std;
 
 class Permutation{
@@ -14,12 +15,14 @@ public:
     vector<string>find_permutation(string s){
         stringLength = s.length();
         originalString = s;
-        vector<int> deletionIndexes;
+        int deletionIndexes[stringLength];
+        int deletionIndexSize = 0;
         string emptyString = "";
-        find_permutation_helper(emptyString, deletionIndexes);
+        srand(time(NULL));
+        find_permutation_helper(emptyString, deletionIndexes, deletionIndexSize);
         return allPermutations;
     }
-    void find_permutation_helper(string s, vector<int> deletionIndexes){
+    void find_permutation_helper(string s, int deletionIndexes[], int deletionIndexSize){
         if(static_cast<int>(s.length()) == stringLength){
             //current permutation has been achieved
             //add it to vector
@@ -28,23 +31,45 @@ public:
         }
         //Might be problematic
 
-        int x;
+        /*int x;
         do{
-            x = rand() % stringLength;
-        }while(contains(x, deletionIndexes));
-        deletionIndexes.push_back(x);
-        s += originalString.at(x);
+            x = rand() % (originalString.length());
+        }while(contains(x, deletionIndexes, deletionIndexSize));
+        deletionIndexes[deletionIndexSize] = x;
+        s += originalString.at(x);*/
         //Recursive step
-        find_permutation_helper(s, deletionIndexes);
+        int boundary = static_cast<int>(-s.length());
+        boundary += stringLength;
+        for(int i = 0; i < boundary; i++){ // switch to <
+            string sCopy = s;
+            int x;
+            do{
+                x = rand() % (originalString.length());
+            }while(contains(x, deletionIndexes, deletionIndexSize));
 
+            //copy the current array
+            int newDeletionIndex[stringLength];
+            copyNewDeletionIndexes(newDeletionIndex, deletionIndexes, stringLength);
+            newDeletionIndex[deletionIndexSize] = x;
+            sCopy += originalString.at(x);
+            find_permutation_helper(sCopy, newDeletionIndex, deletionIndexSize + 1);
+        }
     }
 
-    static bool contains(int x, vector<int> arr){
-        for(size_t i = 0; i < arr.size(); i++){
-            if(arr.at(i) == x){
+    static bool contains(int x, int arr[], int deletionIndexesSize){
+        for(int i = 0; i < deletionIndexesSize; i++){
+            if(arr[i] == x){
                 return true;
             }
         }
         return false;
+    }
+    //copy a new of the given array
+    static void copyNewDeletionIndexes(int copyDeletionIndexes[] ,const int deletionIndexes[], int length){
+        //int copyDeletionIndexes[stringLength];
+        for(int i = 0; i < length; i++){
+            copyDeletionIndexes[i] = deletionIndexes[i];
+        }
+        //return copyDeletionIndexes;
     }
 };
--- GeekProblems/main.cpp
+++ GeekProblems/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
-#include "Solution.cpp"
+//#include "Solution.cpp"
 #include "Permutation.cpp"
 #include <vector>
 #include <string>
 using namespace std;
 
 int main(){
-    Solution sol;
+    //Solution sol;
     Permutation perm;
     vector<string> stringVector;
 
-    stringVector = perm.find_permutation("ABCD");
+    stringVector = perm.find_permutation("ABC");
     for(size_t i = 0; i < stringVector.size(); i++){
-        cout << stringVector.pop_back() << endl;
+        string curr = stringVector.at(i);
+        cout << curr << endl;
     }
     //std::cout << sol.maxGcd(4);
     return 0;
