47b80e613d598a2046e26a22f1766fb96c927dc0
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2022-12-31 21:00:16 +0300

    Some additional adjustements
    1 Step closer to the solution

--- GeekProblems/HowManyLaptops.cpp
+++ GeekProblems/HowManyLaptops.cpp
@@ -8,39 +8,40 @@ public:
         //Find the maximum number of laptops used at any time
         //Do this by calculating the differences between start and ending arrays
         //Çakışan zaman dilimlerini bulmaya çalış
-        int* firstStart = &start[0]; //minimum of the start
-        int* lastEnd = &ending[0]; // maximum of the end
+        int firstStart = start[0]; //minimum of the start
+        int lastEnd = ending[0]; // maximum of the end
         int minimumLaptopCount = 0;
         for(int i = 1; i < N - 1; i++){
             int startCurr = start[i];
             int endCurr = ending[i];
-            int fs1 = *firstStart; // for debugging //PROBLEM HERE
+            int fs1 = firstStart; // for debugging //PROBLEM HERE
 
-            if(startCurr < *firstStart){
-                firstStart = &startCurr;
-                fs1 = *firstStart;
+            if(startCurr < firstStart){
+                firstStart = startCurr;
+                fs1 = firstStart;
 
             }
-            if(endCurr > *lastEnd){
-                lastEnd = &endCurr;
+            if(endCurr > lastEnd){
+                lastEnd = endCurr;
             }
         }
-        while(*firstStart <= *lastEnd){ // change to <
+        while(firstStart <= lastEnd){ // change to <
             int currLaptopCount = 0;
 
             //check each array element
             for(int i = 0; i < N; i++){
-                int fs = *firstStart, ls = *lastEnd;
+                int fs = firstStart, ls = lastEnd;
                 int currStart = start[i];
-                int currEnd = ending[i];
-                if(*firstStart >= currStart && *lastEnd >= currEnd){
+                int currEnd = ending[N - i - 1];
+                if(firstStart < currStart && lastEnd > currEnd){
                     currLaptopCount++;
                 }
             }
             if(currLaptopCount > minimumLaptopCount){
                 minimumLaptopCount = currLaptopCount;
             }
-            *firstStart++;
+            firstStart++;
+            lastEnd--; // maybe remove
         }
         return minimumLaptopCount;
     }
