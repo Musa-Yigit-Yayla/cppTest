a1b6aedc51ee06f917b243237ab4a5a85edcaaef
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-01-03 17:55:48 +0300

    Compileable MinimizeStudents
    All compile time errors fixed. No runtime testing has been made.

--- GeekProblems/MinimizeStudents.cpp
+++ GeekProblems/MinimizeStudents.cpp
@@ -22,17 +22,16 @@ public:
         return result;
     }
     int removeStudent(int heights[], int n){
-        int result;
         //Find all subsequences
         vector<int> emptyVector;
         this->findSubsequences(heights, 0 ,emptyVector, n);
         //Remove non increasing subsequences
         bool x = this->isIncreasingVector(); // clear the non increasing ones
-        this->subsequences.resize();
+        //this->subsequences.resize();
         //find the lengthiest vector
         int maxSize = this->subsequences.at(0).size();
         for(int j = 1; j < this->subsequences.size(); j++){
-            int currSize = this->subsequences.at(i).size();
+            int currSize = this->subsequences.at(j).size();
             if(currSize > maxSize){
                 maxSize = currSize;
             }
@@ -67,19 +66,22 @@ public:
     //Clear the non increasing ones will return true if anything exists
     bool isIncreasingVector(){
         bool returnVal = false;
-        for(int i = 0; i < this->subsequences.size(); i++){
-            vector<int> currentVec = this->subsequences().at(i);
+        for(size_t i = 0; i < this->subsequences.size(); i++){
+            vector<int> currentVec;
+            currentVec = (this->subsequences.at(i));
             int prev = currentVec.at(0);
             int curr;
-            if(vec.size() <= 1){
+            if(currentVec.size() <= 1){
                 continue;
             }
 
-            for(int j = 0; j < currentVec.size(); j++){
+            for(int j = 1; j < currentVec.size(); j++){
                 curr = currentVec.at(j);
                 if(curr < prev){
                     //subsequence doesn't work remove the subsequence at the subsequences vector
-                    this->subsequences.erase(i);
+                    this->subsequences.at(i).clear();
+                    vector<vector<int>>::iterator it = this->subsequences.begin() + i;
+                    this->subsequences.erase(it);
                     i--;
                     break;
                 }
@@ -92,6 +94,7 @@ public:
         }
         return returnVal;
     }
+
     void removeElt(int* arr, int index, int n){
         while(index < n - 1){
             arr[index] = arr[index + 1];
